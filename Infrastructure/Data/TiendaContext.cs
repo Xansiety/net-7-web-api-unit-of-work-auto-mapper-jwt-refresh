f2d1e9cc8dcf64b84c066f9e35433e07158c08bf
xansiety <ferando543@outlook.com>
2022-08-24 21:08:59 -0500

    Configuracion proveedor servicio MySql | Docker | Seed Data | CodeFirst

---
+++ Infrastructure/Data/TiendaContext.cs
@@ -0,0 +1,25 @@
+
+using System.Reflection;
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class TiendaContext : DbContext
+    {
+        public TiendaContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<Marca> Marcas { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
