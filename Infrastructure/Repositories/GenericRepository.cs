6e084b3e3ba388ca10e444a688c729c402a02499
xansiety <ferando543@outlook.com>
2022-08-28 11:29:43 -0500

    Implementacion GenericRepository

---
+++ Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,56 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Infrastructure.Data;
+using System.Linq.Expressions;
+
+namespace Infrastructure.Repositories;
+public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+{
+    protected readonly TiendaContext _context;
+    public GenericRepository(TiendaContext context)
+    {
+        _context = context;
+    }
+
+
+    public virtual void Add(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public virtual void AddRange(IEnumerable<T> entities)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<T> Find(Expression<Func<T, bool>> expression)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<T>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T> GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Remove(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RemoveRange(IEnumerable<T> entities)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(T entity)
+    {
+        throw new NotImplementedException();
+    }
+}
+
