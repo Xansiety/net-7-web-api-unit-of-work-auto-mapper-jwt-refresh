5325d202c1773ba39d45e9edb5926b3360ec9078
xansiety <ferando543@outlook.com>
2022-08-28 12:34:55 -0500

    Controlador con unidad de trabajo

--- API/Controllers/ProductosController.cs
+++ API/Controllers/ProductosController.cs
@@ -34,5 +34,50 @@ namespace API.Controllers
             var productos = await _unityOfWork.Productos.GetByIdAsync(id);
             return Ok(productos);
         }
+
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<Producto>> Post(Producto producto)
+        {
+            _unityOfWork.Productos.Add(producto);
+            _unityOfWork.Save();
+            if (producto is null) return BadRequest();
+
+            return CreatedAtAction(nameof(Post), new { id = producto.Id }, producto);
+        }
+
+
+        //PUT: api/productos/28
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<Producto>> Put(int id, [FromBody] Producto producto)
+        {
+            if (producto is null) return NotFound();
+
+            _unityOfWork.Productos.Update(producto);
+            _unityOfWork.Save();
+            if (producto is null) return BadRequest();
+
+            return producto;
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var producto = await _unityOfWork.Productos.GetByIdAsync(id);
+            if (producto is null) return NotFound();
+
+            _unityOfWork.Productos.Remove(producto);
+            _unityOfWork.Save();
+
+            return NoContent();
+
+        }
     }
 }
