a82cd16e513dd590a25e01e669457e057aa5fe91
xansiety <ferando543@outlook.com>
2023-06-24 21:56:23 -0600

    feature(AsNoTracking()) Implement AsNoTracking in ProductRepository

--- Infrastructure/Repositories/ProductoRepository.cs
+++ Infrastructure/Repositories/ProductoRepository.cs
@@ -18,17 +18,25 @@ namespace Infrastructure.Repositories
             .ToListAsync();
 
 
-        public override async Task<Producto> GetByIdAsync(int id)
+        public override async Task<Producto> GetByIdAsync(int id, bool noTracking = true)
         {
-            return await _context.Productos
+
+            var queryProducto = noTracking ? _context.Productos.AsNoTracking()
+                                           : _context.Productos;
+
+            return await queryProducto
                 .Include(p => p.Marca)
                 .Include(p => p.Categoria)
                 .FirstOrDefaultAsync(p => p.Id == id);
         }
-         
-        public override async Task<IEnumerable<Producto>> GetAllAsync()
+
+        public override async Task<IEnumerable<Producto>> GetAllAsync(bool noTracking = true)
         {
-            return await _context.Productos
+
+            var queryProducto = noTracking ? _context.Productos.AsNoTracking()
+                                          : _context.Productos;
+
+            return await queryProducto
                 .Include(u => u.Marca)
                 .Include(u => u.Categoria)
                 .ToListAsync();
@@ -36,20 +44,23 @@ namespace Infrastructure.Repositories
 
 
         public override async Task<(int totalRegistros, IEnumerable<Producto> registros)> GetAllAsync(
-                 int pageIndex, int pageSize, string search)
+                 int pageIndex, int pageSize, string search, bool noTracking = true)
         {
-            var consulta = _context.Productos as IQueryable<Producto>;
+
+            var queryProducto = noTracking ? _context.Productos.AsNoTracking()
+                                         : _context.Productos;
+            //var consulta = _context.Productos as IQueryable<Producto>;
 
             if (!String.IsNullOrEmpty(search))
             {
-                consulta = consulta.Where(p => p.Nombre.ToLower().Contains(search));
+                queryProducto = queryProducto.Where(p => p.Nombre.ToLower().Contains(search));
             }
 
 
-            var totalRegistros = await consulta
+            var totalRegistros = await queryProducto
                                         .CountAsync();
 
-            var registros = await consulta
+            var registros = await queryProducto
                                     .Include(u => u.Marca)
                                     .Include(u => u.Categoria)
                                     .Skip((pageIndex - 1) * pageSize)
